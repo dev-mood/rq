58fed6d5970a29aa2547ca23926e2e53a2a2eed8
Roberth <roberthgon18@gmail.com>
2020-06-29 23:44:01 +0200

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="version">v1.0.0</div>
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.version {
+  position: fixed;
+  top: 1%;
+  right: 1%;
+  display: inline-block;
+  font-weight: 500;
+  font-family: inherit;
+  line-height: inherit;
+  text-align: center;
+  -webkit-appearance: none;
+  cursor: pointer;
+  color: #fff;
+  background-color: #ffdd09;
+  box-shadow: inset 0 0 0 1px #ffdd09;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-left: 8px;
+  padding-right: 8px;
+  font-size: 14px;
+  margin: 0px 0px 0px 8px;
+  text-decoration: none;
+  border-width: 0px;
+  border-style: initial;
+  border-color: initial;
+  border-image: initial;
+  border-radius: 6px;
+  user-select: none;
+}
+
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #ffdd09;
+  color: #ffdd09;
+  text-decoration: none;
+  padding: 0.3rem 0.6rem;
+  margin-left: 0.3rem;
+  background: transparent;
+  cursor: pointer;
+  &:hover {
+    color: #fff;
+    background-color: #ffdd09;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="/logo.png" alt="Logo" width="300" />
+      <div class="links">
+        <button v-for="(link, index) in links" :key="index" class="button">{{link.emoji}}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      links: [
+        {
+          emoji: '🔥'
+        },
+        {
+          emoji: '😄'
+        },
+        {
+          emoji: '❤️'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.links {
+  padding-top: 15px;
+}
+</style>
